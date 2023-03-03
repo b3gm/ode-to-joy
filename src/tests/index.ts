2f70309d78479192f7bf96803806cac399451620
Patrick Günther <guenther@swissquant.com>
2023-03-03 13:38:17 +0100

    wip, committs unsaved changes

--- src/tests/index.ts
+++ src/tests/index.ts
@@ -1,5 +1,14 @@
 import * as three from "three";
+import { Vector3 } from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { ABType } from "../array-backed-types/ab-type";
+import { heunMethod, midPointMethod } from "../explicit-solvers";
+import { createGenericExplicitSolver } from "../generic-explicit-solver";
+import { abTypes } from "../index";
+import { abSolarSystem } from "./ab-solar-system";
+import { Body } from "./body";
+import { createSolarSystem } from "./create-solar-system";
+import { SolarSystem } from "./solar-system";
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Dom ready!");
@@ -7,6 +16,27 @@ document.addEventListener("DOMContentLoaded", () => {
   if (!cvs) {
     throw new Error("Canvas not found.");
   }
+  const solarSystem = createSolarSystem();
+  const {celestialBodies, asteroids, gravityConstant} = solarSystem;
+  const scene = new three.Scene();
+  celestialBodies.forEach(body => scene.add(body.mesh));
+  asteroids.forEach(ast => scene.add(ast.mesh));
+  const camera = new three.PerspectiveCamera(
+    65.0,
+    cvs.width / cvs.height,
+    0.1,
+    10000.0
+  );
+
+  const solarSystemSolver = createGenericExplicitSolver<SolarSystem>(
+    abSolarSystem(solarSystem),
+    midPointMethod
+  );
+
+  function loop() {
+    
+  }
+
 
   
 
